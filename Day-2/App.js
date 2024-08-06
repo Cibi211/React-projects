36c21c3a814e8b717549bccd3ed459b3f11ee5fe
Cibi211 <150768302+Cibi211@users.noreply.github.com>
2024-08-06 20:56:01 +0530

    Add files via upload

--- Day-2/App.js
+++ Day-2/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-const Greet = () =>
-{
+const App8 = () =>{
     return(
-        <div>
-            <h1 style={{textAlign:"center"}}>Smile Component</h1>
-            <p style={{textAlign:"center"}}>It is a functional component</p>
-            <img src="https://media.istockphoto.com/id/1388186992/vector/yellow-happy-face-vector-symbol-icon.jpg?s=612x612&w=0&k=20&c=GZBWkt0ZLxLHkxU58BhRqkk41xC0LySjXa0N65xMDlw=" alt="Smiling face" ></img>
+        <div style={{backgroundColor:"lightblue"}}>
+            <h1 style={{textAlign:"center"}}>Car and Location Info</h1>
+            
+            
+            <h3 style={{textAlign:"center"}}>Tesla Model S</h3>
+            <img src="https://www.google.com/imgres?q=tesla%20car&imgurl=https%3A%2F%2Fwww.digitaltrends.com%2Fwp-content%2Fuploads%2F2020%2F06%2Fmodel-3-performance-red-motion.jpg%3Fresize%3D650%252C390%26p%3D1&imgrefurl=https%3A%2F%2Fwww.digitaltrends.com%2Fcars%2Ftesla-autopilot-vs-full-self-driving%2F&docid=kT75x8McVI_-aM&tbnid=_8sPPG3vDhSSDM&vet=12ahUKEwj2w9ia1uCHAxUfTmwGHf5eERkQM3oECC8QAA..i&w=650&h=390&hcb=2&ved=2ahUKEwj2w9ia1uCHAxUfTmwGHf5eERkQM3oECC8QAA" alt="Tesla Model S" style={{alignItems:"center"}}></img>
+            <h3 style={{textAlign:"center"}}>Tesla</h3>
+            <p style={{textAlign:"center"}}>Palo Alto</p>
         </div>
     );
 };
-export default Greet;
+export default App8;
