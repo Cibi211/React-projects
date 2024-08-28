d593367bb48c4e386d2d465b22ffaf40c5ea74db
Cibi211 <150768302+Cibi211@users.noreply.github.com>
2024-08-28 21:16:00 +0530

    Add files via upload

---
+++ Day3/Tamle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToggleMessage from './ToggleMessage';
+
+
+function Tamle() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Messaging Dashboard</h1>
+        <ToggleMessage />
+      </header>
+    </div>
+  );
+}
+
+export default Tamle;
